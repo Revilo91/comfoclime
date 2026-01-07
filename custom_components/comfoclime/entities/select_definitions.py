abd89ffcba6a21247eaf6a67fbfe4502be181898
Revilo91 <oliver.suedholt@gmail.com>
2026-01-07 15:02:13 +0100

    Füge neue Optionen für passive Temperatur und Temperaturprofil Modus hinzu

--- custom_components/comfoclime/entities/select_definitions.py
+++ custom_components/comfoclime/entities/select_definitions.py
@@ -16,16 +16,34 @@ SELECT_ENTITIES = [
 PROPERTY_SELECT_ENTITIES = {
     1: [
         {
+            "path": "29/1/4",
+            "name": "Passive Temperature",
+            "translation_key": "passive_temperature",
+            "options": {0: "off", 1: "autoonly", 2: "on"},
+        },
+        {
             "path": "29/1/6",
             "name": "Humidity Comfort Control",
             "translation_key": "humidity_comfort_control",
             "options": {0: "off", 1: "autoonly", 2: "on"},
         },
         {
-            "path": "29/1/7",  # X/Y/Z
+            "path": "29/1/7",
             "name": "Humidity Protection",
             "translation_key": "humidity_protection",
             "options": {0: "off", 1: "autoonly", 2: "on"},
+        },
+        {
+            "path": "29/1/8",
+            "name": "Temperature Profile Modus",
+            "translation_key": "temperature_profile_modus",
+            "options": {0: "adaptiv", 1: "fixed value"},
+        },
+        {
+            "path": "29/1/14",
+            "name": "Temp Passively Preset",
+            "translation_key": "temp_passively_preset",
+            "options": {0: "slow", 1: "medium", 2: "fast"},
         },
     ]
 }
